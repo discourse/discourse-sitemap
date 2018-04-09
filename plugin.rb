52ae091d5bb82070bf4670cfa7a7e0978c812578
Sam <sam.saffron@gmail.com>
2018-04-09 15:48:56 +1000

    Improve ordering of sitemap, old cached sitemap pages should be crawled less frequently, also add a "recent.xml" for all activity in last 24 hours

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: Discourse Sitemap
 # about:
 # version: 1.1
-# authors: DiscourseHosting.com, vinothkannans 
+# authors: DiscourseHosting.com, vinothkannans
 # url: https://github.com/discoursehosting/discourse-sitemap
 
 PLUGIN_NAME = "discourse-sitemap".freeze
@@ -26,8 +26,12 @@ after_initialize do
     def topics_query(since = nil)
       category_ids = Category.where(read_restricted: false).pluck(:id)
       query = Topic.where(category_id: category_ids, visible: true)
-      query = query.created_since(since) unless since.nil?
-      query = query.order(created_at: :desc)
+      if since
+        query = query.where('last_posted_at > ?', since)
+        query = query.order(last_posted_at: :desc)
+      else
+        query = query.order(last_posted_at: :asc)
+      end
       query
     end
 
@@ -36,21 +40,19 @@ after_initialize do
       prepend_view_path "plugins/discourse-sitemap/app/views/"
 
       sitemap_size = SiteSetting.sitemap_topics_per_page
-      @output = Rails.cache.fetch("sitemap/index/#{sitemap_size}", expires_in: 24.hours) do
+      @output = Rails.cache.fetch("sitemap/index/v2/#{sitemap_size}", expires_in: 24.hours) do
         count = topics_query.count
         @size = count / sitemap_size
         @size += 1 if count % sitemap_size > 0
-        @lastmod = Time.now
+
+        # grabbing full sitemap more than once a day seems too much
+        @lastmod = 1.day.ago
         1.upto(@size) do |i|
           Rails.cache.delete("sitemap/#{i}")
         end
-        if @size > 1
-          render :index, content_type: 'text/xml; charset=UTF-8'
-        else
-          sitemap(1)
-        end
+        render :index, content_type: 'text/xml; charset=UTF-8'
       end
-      render :plain => @output, content_type: 'text/xml; charset=UTF-8' unless performed?
+      render plain: @output, content_type: 'text/xml; charset=UTF-8' unless performed?
     end
 
     def default
@@ -59,6 +61,24 @@ after_initialize do
 
       page = Integer(params.require(:page))
       sitemap(page)
+    end
+
+    def recent
+      raise ActionController::RoutingError.new('Not Found') unless SiteSetting.sitemap_enabled
+      prepend_view_path "plugins/discourse-sitemap/app/views/"
+
+      sitemap_size = SiteSetting.sitemap_topics_per_page
+
+      @output = Rails.cache.fetch("sitemap/recent", expires_in: 5.minutes) do
+        @topics = Array.new
+        topics_query(3.days.ago).limit(sitemap_size).pluck(:id, :slug, :last_posted_at, :updated_at, :posts_count).each do |t|
+          t[2] = t[3] if t[2].nil?
+          @topics.push t
+        end
+        render :default, content_type: 'text/xml; charset=UTF-8'
+      end
+      render plain: @output, content_type: 'text/xml; charset=UTF-8' unless performed?
+      return @output
     end
 
     def sitemap(page)
@@ -73,7 +93,7 @@ after_initialize do
         end
         render :default, content_type: 'text/xml; charset=UTF-8'
       end
-      render :plain => @output, content_type: 'text/xml; charset=UTF-8' unless performed?
+      render plain: @output, content_type: 'text/xml; charset=UTF-8' unless performed?
       return @output
     end
 
@@ -88,7 +108,7 @@ after_initialize do
         @topics = topics_query(72.hours.ago).pluck(:id, :title, :slug, :created_at)
         render :news, content_type: 'text/xml; charset=UTF-8'
       end
-      render :plain => @output, content_type: 'text/xml; charset=UTF-8' unless performed?
+      render plain: @output, content_type: 'text/xml; charset=UTF-8' unless performed?
     end
   end
 
@@ -99,6 +119,7 @@ after_initialize do
   DiscourseSitemap::Engine.routes.draw do
     get ".xml" => "sitemap#index"
     get "news.xml" => "sitemap#news"
+    get "recent.xml" => "sitemap#recent"
     get ":page.xml" => "sitemap#default"
   end
 
